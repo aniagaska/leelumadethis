05181ba6287390a82664088c6466a055a57be2d0
Ania <44368856+aniagaska@users.noreply.github.com>
2024-04-06 12:29:53 -0400

    Load random image from images folder
    I've added the code from codepen, and the names of the folder but it's not working for me on my computer!

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ leelumadethis.lol
 
 ## Aly is a great writer
 A fan site
+# Aly is a great writer 
+Aly is a great writer is a website comissioned by her beautiful wife, Emily, who lovingly went through reviews of her books and took screen shots of the good ones. This way, Aly gets the encouragement she needs without having to witness the terrors of the internet. 
+
+## Leelu made this
+The domain is [leelumadethis.lol](http://leelumadethis.lol) because Leelu is a cat and the lol was available. Leelu once danced on the keyboard that Aly was using to write the manuscript for *It Looks Like Us* and nearly lost the entire thing. She also set Aly's author photo as the desktop background! The manuscript was recovered and Leelu was thwarted again. 
+
+### Make this for your own loved one, who happens to write books
+If you know someone who makes things that gets reviews online, please feel free to follow Emily's example and screenshot the good ones. 
--- main.js
+++ main.js
@@ -1,2 +1,40 @@
 // this is fine i'm not even scared
-alert("hello")
+//alert("hello")
+
+// Chris made the random image example https://codepen.io/bronzehedwick/pen/dyLdJmG 
+// Load random image from images folder on load
+(function main() {
+    'use strict';
+  
+    window.addEventListener('load', function handleLoad() {
+      const imagePaths = [
+        '/images/review001.jpg',
+        '/images/review002.jpg',
+        '/images/review003.jpg',
+        '/images/review004.jpg',
+        '/images/review005.jpg',
+        '/images/review006.jpg',
+        '/images/review007.jpg',
+        '/images/review008.jpg',
+        '/images/review009.jpg',
+        '/images/review010.jpg',
+        '/images/review011.jpg',
+        '/images/review012.jpg',
+        '/images/review013.jpg',
+        '/images/review014.jpg',
+        '/images/review015.jpg',
+        '/images/review016.jpg',
+        '/images/review017.jpg',
+        '/images/review018.jpg',
+        '/images/review019.jpg',
+        '/images/review020.jpg',
+      ];
+  
+      const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+      const imageContainer = document.getElementById('image-container');
+      const randomImage = new Image();
+      randomImage.src = randomImagePath;
+      randomImage.alt = '';
+      imageContainer.appendChild(randomImage);
+    });
+  })();
